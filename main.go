fc943bf5bd72df8cc46cd4a8a0b1296505b71bc9
HoangDavid <hoangnguyenxuan2004@gmail.com>
2024-11-26 13:31:56 -0500

    add sendStream and streamResults

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/stream/start", startStream).Methods("POST")
+	r.HandleFunc("/stream/{stream_id}/send", sendStream).Methods("POST")
+	r.HandleFunc("/stream/{stream_id}/results", streamResults).Methods("GET")
 
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", r); err != nil {
@@ -23,6 +25,19 @@ func startStream(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Hello, I am ur first working stream in Golang\n"))
 }
 
+func sendStream(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	stream_id := vars["stream_id"]
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Stream " + stream_id + " sent\n"))
+}
+
+func streamResults(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	stream_id := vars["stream_id"]
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Show stream results"))
+}
 // Testing:
 // Run: -X POST http://localhost:8080/stream/start
 //
