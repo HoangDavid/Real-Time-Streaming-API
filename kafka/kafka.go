3955507b0127cbfd7f3e26944811e8e4a71adf4b
HoangDavid <hoangnguyenxuan2004@gmail.com>
2024-11-27 14:32:50 -0500

    integrated Kafka(Redpanda)

---
+++ kafka/kafka.go
@@ -0,0 +1,55 @@
+package kafka
+
+import (
+	"fmt"
+	"context"
+	"log"
+	"time"
+	"github.com/segmentio/kafka-go"
+)
+
+var writer *kafka.Writer
+
+func InitKafka() {
+	writer = kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic: "default",
+		Balancer: &kafka.LeastBytes{},
+	})
+}
+
+func ProduceMessage(topic string, message string) error {
+	err := writer.WriteMessages(context.Background(),
+		kafka.Message{
+			Key: []byte(fmt.Sprintf("Key-%d", time.Now().Unix())),
+			Value: []byte(message),
+		},
+	)
+	
+	return err
+}
+
+func ConsumeMessage(topic string) {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic: topic,
+		Partition: 0,
+		MinBytes: 10e3,
+		MaxBytes: 10e6,
+	})
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil{
+			log.Printf("could not read message %v", err)
+			continue
+		}
+
+		fmt.Printf("Message: %s\n", string(m.Value))
+	}
+}
+
+//To create a topic for Kafka:
+//	docker exec -it redpanda rpk topic create default 
+//To run redpanda on Docker:
+//  docker run -d --name=redpanda -p 9092:9092 -p 9644:9644 docker.redpanda.com/vectorized/redpanda:latest redpanda start --overprovisioned --smp 1 --memory 1G --reserve-memory 0M --node-id 0 --check=false --kafka-addr PLAINTEXT://0.0.0.0:9092 --advertise-kafka-addr PLAINTEXT://localhost:9092
--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import (
 	"net/http"
 	// "github.com/gorilla/websocket"
 	"github.com/gorilla/mux"
+	"real-time-api/kafka"
 )
 
 func main() {
+	kafka.InitKafka()
+
 	r := mux.NewRouter()
 	r.HandleFunc("/stream/start", startStream).Methods("POST")
 	r.HandleFunc("/stream/{stream_id}/send", sendStream).Methods("POST")
@@ -27,17 +30,30 @@ func startStream(w http.ResponseWriter, r *http.Request){
 
 func sendStream(w http.ResponseWriter, r *http.Request){
 	vars := mux.Vars(r)
-	stream_id := vars["stream_id"]
+    streamID := vars["stream_id"]
+
+    err := kafka.ProduceMessage(streamID, "Sample data chunk")
+    if err != nil {
+        http.Error(w, "Failed to send data to Kafka\n" + err.Error() + "\n", http.StatusInternalServerError)
+        return
+    }
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Stream " + stream_id + " sent\n"))
+	w.Write([]byte("Stream sent!\n"))
 }
 
 func streamResults(w http.ResponseWriter, r *http.Request){
 	vars := mux.Vars(r)
-	stream_id := vars["stream_id"]
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Show stream results"))
+    streamID := vars["stream_id"]
+
+    go kafka.ConsumeMessage(streamID)
+
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("Consuming stream results\n"))
 }
+
+
 // Testing:
 // Run: -X POST http://localhost:8080/stream/start
 //
+
+// What is  a kafka topic? idk
